7ad6acb65da92b36b493a36a88bd65bc0820260e
david-kolundzic <david-kolundzic@outlook.com>
2025-07-07 10:38:01 +0200

    MAIN COMMIT
    PRVI COMMIT
    signal<Product | undefined>()update(), set(), effect(()=>{})

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, Pipe, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductData } from '../product-data';
+import { Product } from '../product';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
 
+  selectedProduct = signal<Product | undefined>(undefined)
+  quantity = signal(1);
+
+  products = signal(ProductData.products);
+
+  onDecrease() {
+    this.quantity.update(qty => qty > 0 ? qty - 1 : 0); // Ensure quantity does not go below 1
+  }
+
+  onIncrease() {
+    // this.quantity.set(1); // Reset to 1 before increasing
+    this.quantity.update(qty => qty + 1);
+  
+  }
+
+  qtyEffect= effect(() => {
+    console.log(`Quantity changed to: ${this.quantity()}`);
+  })
+  
 }
